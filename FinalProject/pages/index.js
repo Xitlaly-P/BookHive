1d8b0e7a1a77df2118f62fb6117e89b5c3289ecb
sarahdob <sd869@njit.edu>
2024-08-02 14:07:44 -0400

    initial project setup

---
+++ FinalProject/pages/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>The Book Hive</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
